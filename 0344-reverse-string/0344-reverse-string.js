724ab1a6fe6c5a588ca68a97a745f68393c529b0
Roland Estropia Jr <66459527+restropiajr@users.noreply.github.com>
2023-04-18 02:26:35 -0700

    Time: 147 ms (5.03%), Space: 53.2 MB (5.26%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.js
@@ -0,0 +1,10 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+	
+
+	const reverseStr = s.reverse();
+	console.log(reverseStr);
+};
