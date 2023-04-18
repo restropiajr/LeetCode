2850bda251c1ec69cb93977040f29f8182a28748
Roland Estropia Jr <66459527+restropiajr@users.noreply.github.com>
2023-04-18 00:52:51 -0700

    Time: 60 ms (48.70%), Space: 42.3 MB (16.98%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} celsius
+ * @return {number[]}
+ */
+var convertTemperature = function(celsius) {
+    
+    let arr = [];
+    
+    arr.push(celsius + 273.15);
+    arr.push(celsius * 1.80 + 32);
+    
+    return arr;
+    
+};
