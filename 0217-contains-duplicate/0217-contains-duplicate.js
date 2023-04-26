ab4dc381901c9bde6f634f3d554d1348b9f32432
Roland Estropia Jr <66459527+restropiajr@users.noreply.github.com>
2023-04-26 00:34:57 -0700

    Time: 189 ms (19.78%), Space: 51.9 MB (86.13%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+var containsDuplicate = function (nums) {
+	let sortedArr = nums.sort();
+
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] === nums[i + 1]) {
+			return true;
+		}
+	}
+	return false;
+};
