537c5b76a66f78048164a7f785c32ee84193e46e
Roland Estropia Jr <66459527+restropiajr@users.noreply.github.com>
2023-04-26 01:31:37 -0700

    Time: 63 ms (77.45%), Space: 43 MB (93.90%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -8,7 +8,6 @@ var majorityElement = function(nums) {
     let majorityElement = 0;
     
     for (let i = 0; i < nums.length; i++) {
-        
         if (count === 0) {
             majorityElement = nums[i];
             count++;
